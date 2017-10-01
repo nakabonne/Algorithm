96d19a40299a30d4f9266c2eb9a302a0f8bfcd28
ryonakao <rodriguez.nak@gmail.com>
2017-10-01 13:29:59 +0900

    Add binary_search.c

---
+++ AOJ/search/binary_search.rb
@@ -0,0 +1,24 @@
+n = gets.to_i
+s = gets.split.map(&:to_i)
+q = gets.to_i
+t = gets.split.map(&:to_i)
+
+sum = 0
+
+for i in 0..(q-1)
+  left = 0
+  right = n-1
+  key = t[i]
+  while left < right
+    mid = (right+left)/2
+    if key == s[mid]
+      sum += 1
+      break
+    elsif key < s[mid]
+      right = mid
+    else
+      left = mid+1
+    end
+  end
+end
+puts sum
